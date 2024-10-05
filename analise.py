3e9cd9bb4d3e1e6c65061726713794d3b519323e
Felipe Fontes <116237840+felipefontesoficial@users.noreply.github.com>
2024-10-05 05:27:03 -0300

    Update analise.py

--- analise.py
+++ analise.py
@@ -15,7 +15,6 @@ with col1:
     ligacoes = st.number_input('Quantas ligações foram feitas?', min_value=0)
     atendidas = st.number_input('Quantas ligações foram atendidas?', min_value=0)
     reunioes_agendadas = st.number_input('Quantas reuniões foram agendadas?', min_value=0)
-    vendas_realizadas = st.number_input('Quantas vendas foram realizadas?', min_value=0)
 
     if st.button('Calcular'):
         # Cálculos das taxas de conversão
@@ -29,20 +28,14 @@ with col1:
         else:
             taxa_reuniao = 0
 
-        if reunioes_agendadas > 0:
-            taxa_fechamento = (vendas_realizadas / reunioes_agendadas) * 100
-        else:
-            taxa_fechamento = 0
+
 
        
 
 # Coluna 2 - Funil de Vendas Visual
 with col2:
      # Exibir as taxas de conversão calculadas
-        st.subheader('Resultados')
-        st.write(f'Taxa de Conversão de Ligações Atendidas: {taxa_atendimento:.2f}%')
-        st.write(f'Taxa de Conversão de Reuniões Agendadas: {taxa_reuniao:.2f}%')
-        st.write(f'Taxa de Conversão de Reuniões para Vendas: {taxa_fechamento:.2f}%')
+
 
         # Avaliação e recomendações
         st.subheader('Avaliação e Recomendações:')
@@ -59,8 +52,12 @@ with col2:
         else:
             st.success("A taxa de agendamento de reuniões está no nível recomendado!")
 
-        if taxa_fechamento < 35:
-            st.warning("A taxa de conversão de reuniões para vendas está abaixo da média (35%). Foque em melhorar a qualificação dos leads durante as reuniões.")
-        else:
-            st.success("Ótima taxa de conversão em vendas!")
-            
+
+try:
+        st.subheader('Resultados')
+        st.write(f'Taxa de Conversão de Ligações Atendidas: {taxa_atendimento:.2f}%')
+        st.write(f'Taxa de Conversão de Reuniões Agendadas: {taxa_reuniao:.2f}%')
+        st.write(f'Taxa de Conversão de Reuniões para Vendas: {taxa_fechamento:.2f}%')
+
+except:
+    print("Vamos começar?")
