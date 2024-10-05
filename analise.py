0793997aca2b3af359d69c6302903616099a13c2
Felipe Fontes <116237840+felipefontesoficial@users.noreply.github.com>
2024-10-05 13:49:45 -0300

    Update analise.py

--- analise.py
+++ analise.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
+
+#usuarios = ["Felipe Fontes", "Jullia Nascimento"]
+#atividades = ["Pesquisa", "Ligação","Mensagem", ,]
 
 # Título
 st.title('Calculadora Mivus de Funil')
@@ -12,8 +16,8 @@ with col1:
     st.header('Insira seus números')
     
     # Entradas
-    ligacoes = st.number_input('Quantas ligações foram feitas?', min_value=0)
-    atendidas = st.number_input('Quantas ligações foram atendidas?', min_value=0)
+    ligacoes = st.number_input('Quantas ligações ou abordagens foram feitas?', min_value=0)
+    atendidas = st.number_input('Quantas ligações foram atendidas (ou abordagens respondidas)?', min_value=0)
     reunioes_agendadas = st.number_input('Quantas reuniões foram agendadas?', min_value=0)
 
     if st.button('Calcular'):
@@ -29,20 +33,28 @@ with col1:
             taxa_reuniao = 0
 
 
+# Cálculos das taxas de conversão
+if ligacoes > 0:
+    taxa_atendimento = (atendidas / ligacoes) * 100
+else:
+    taxa_atendimento = 0
 
+if atendidas > 0:
+    taxa_reuniao = (reunioes_agendadas / atendidas) * 100
+else:
+    taxa_reuniao = 0
        
 
 # Coluna 2 - Funil de Vendas Visual
 with col2:
-     # Exibir as taxas de conversão calculadas
-
-
         # Avaliação e recomendações
         st.subheader('Avaliação e Recomendações:')
-        if ligacoes < 50:
-            st.warning("Você abordou menos que o recomendado. Tente abordar entre 50 a 100 novas pessoas semanalmente.")
-        
-        if taxa_atendimento < 30:
+        if ligacoes < 20:
+            st.warning("Você abordou menos que o recomendado. Tente abordar entre 20 a 50 pessoas por dia.")
+        else:
+            st.success("Bateu a meta do dia :D")
+
+        if taxa_atendimento < 25:
             st.warning("A taxa de atendimento está abaixo da média (30%). Considere revisar a qualidade das suas listas ou melhorar o script de abordagem.")
         else:
             st.success("A taxa de atendimento está boa!")
@@ -53,11 +65,11 @@ with col2:
             st.success("A taxa de agendamento de reuniões está no nível recomendado!")
 
 
-try:
+            
+     # Exibir as taxas de conversão calculadas
         st.subheader('Resultados')
-        st.write(f'Taxa de Conversão de Ligações Atendidas: {taxa_atendimento:.2f}%')
-        st.write(f'Taxa de Conversão de Reuniões Agendadas: {taxa_reuniao:.2f}%')
-        st.write(f'Taxa de Conversão de Reuniões para Vendas: {taxa_fechamento:.2f}%')
-
-except:
-    print("Vamos começar?")
+        if taxa_atendimento == 0:
+            st.write('Aguardando dados...')
+        else:
+            st.write(f'Taxa de Conversão de Ligações Atendidas: {taxa_atendimento:.2f}%')
+            st.write(f'Taxa de Conversão de Reuniões Agendadas: {taxa_reuniao:.2f}%')
